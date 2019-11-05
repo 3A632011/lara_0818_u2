bec05a23fadbb3ca94f7d4863ea80897c2145734
3A632011 <abc4173727@gmail.com>
2019-11-05 23:27:00 +0800

    2-1-c:編輯 master.blade.php

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,1 +1,11 @@
-<?php
+<html>
+<head>
+    <title>@yield('title')|MyBlog</title>
+</head>
+<body>
+@include('layouts.partials.navigation')
+<div class="container">
+    @yield('content')
+</div>
+</body>
+</html>
